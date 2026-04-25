1dceae698505f3d817414e49c6a6b48b77054dad
dino060311 <ella060311@gmail.com>
2026-04-25 22:13:55 +0900

    feat: 채팅 이벤트 리스너 추가

--- src/main/java/io/github/dino060311/Main.java
+++ src/main/java/io/github/dino060311/Main.java
@@ -11,6 +11,11 @@ public class Main extends JavaPlugin {
         getLogger().info("MyFirstMCPlugin 활성화 완료!");
         getLogger().info("첫 번째 마인크래프트 플러그인 제작에 성공했습니다.");
         getLogger().info("------------------------------------");
+
+        // 리스너 등록
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
+
+        getLogger().info("채팅 이벤트 리스너가 성공적으로 등록되었습니다.");
     }
 
     @Override
