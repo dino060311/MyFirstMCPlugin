8930990545ef0284239bd2eae75631c8270f368e
dino060311 <ella060311@gmail.com>
2026-05-18 17:41:52 +0900

    fix: 보스 스킬 및 메시지 중복 실행 문제 해결

--- src/main/java/io/github/dino060311/listener/BossListener.java
+++ src/main/java/io/github/dino060311/listener/BossListener.java
@@ -6,7 +6,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.title.Title;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer; // 추가됨
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.boss.BarColor;
 import org.bukkit.entity.LivingEntity;
@@ -64,10 +64,6 @@ public class BossListener implements Listener {
                     // 스킬 발동 및 보스바 변경
                     skillService.areaPush(boss.getLocation());
                     BossManager.updatePhase("§6§l[2페이즈] 분노한 보스 좀비", BarColor.YELLOW);
-
-                    // 스킬 발동 및 보스바 변경
-                    skillService.areaPush(boss.getLocation());
-                    BossManager.updatePhase("§6§l[2페이즈] 분노한 보스 좀비", BarColor.YELLOW);
                 } else if (healthPercent <= 0.2 && !name.contains("[최종페이즈]")) {
                     // 체력이 20% 이하로 떨어졌고, 아직 최종페이즈로 변신하지 않았다면
 
@@ -90,10 +86,6 @@ public class BossListener implements Listener {
                                                 Duration.ofSeconds(4),
                                                 Duration.ofMillis(1000))));
                     }
-
-                    // 스킬 발동 및 보스바 변경
-                    skillService.lastResort(boss.getLocation());
-                    BossManager.updatePhase("§d§l[최종페이즈] 각성한 보스 좀비", BarColor.PURPLE);
 
                     // 스킬 발동 및 보스바 변경
                     skillService.lastResort(boss.getLocation());
