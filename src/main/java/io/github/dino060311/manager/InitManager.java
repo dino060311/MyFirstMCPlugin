330ee0181b40b64c99b4b5cd00d425a6d404b2df
dino060311 <ella060311@gmail.com>
2026-05-22 14:01:32 +0900

    refactor: InteractListener 제거 및 이벤트 구조 정리

--- src/main/java/io/github/dino060311/manager/InitManager.java
+++ src/main/java/io/github/dino060311/manager/InitManager.java
@@ -42,7 +42,6 @@ public class InitManager {
 
         // 시스템 관련
         pm.registerEvents(new ChatListener(plugin), plugin);
-        pm.registerEvents(new InteractListener(), plugin);
         pm.registerEvents(new JoinListener(), plugin);
 
         // 아이템 및 기능 관련
